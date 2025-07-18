d809611bfad56ca4f347dbe5ff46de487e333cfc
Deviousdivya <1407divyabharat@gmail.com>
2025-07-18 14:09:06 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "Divya@google.com"
+var accountPassword = "12345"
+accountCity = "Meerut"
+
+// accoutId = 2 // not allowed
+
+accountEmail = "db@db.com"
+accountPassword = "141414141"
+accountCity = "Vrindavan"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
