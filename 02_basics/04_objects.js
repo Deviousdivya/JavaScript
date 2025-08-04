155882e7424944fcd1044a1bf1c3fb3b387dc154
Deviousdivya <1407divyabharat@gmail.com>
2025-08-04 11:56:39 +0000

    container config done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,61 @@
+// const tinderUser = new Object() //creating new object using Object constructor 
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullname : {
+        username : {
+            firstname : "Divya",
+            lastname : "Bharat"
+        }
+    }
+
+}
+
+// console.log(regularUser.fullname.username.lastname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// console.log(obj3) // same problem as in array , they didn't join (output = { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } })
+
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4) //{} is the target and other objects are source [Object.assign(target,source)]
+// console.log(obj3)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser)); // returns an array of key-values subarrays ([ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ])
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
